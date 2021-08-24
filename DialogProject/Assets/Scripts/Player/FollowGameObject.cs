da43b16fe57e87427a15b663a9b262ed5eab9226
taccck <82225892+taccck@users.noreply.github.com>
2021-08-24 17:29:34 +0200

    Graphical improvments
    added lighting and a background

--- DialogProject/Assets/Scripts/Player/FollowGameObject.cs
+++ DialogProject/Assets/Scripts/Player/FollowGameObject.cs
@@ -3,11 +3,17 @@ using UnityEngine;
 public class FollowGameObject : MonoBehaviour
 {
     [SerializeField] private Transform toFollow;
-    [SerializeField] private float speed = .75f;
+    [SerializeField] private float smoothing = .75f;
+    [SerializeField] private bool fixedX;
 
     private void FixedUpdate()
     {
-        transform.position = Vector2.Lerp(toFollow.position, transform.position, speed);
-        //transform.position = new Vector3(transform.position.x, transform.position.y, -10);
+        if (fixedX)
+        {
+            transform.position = new Vector3(toFollow.position.x, transform.position.y, transform.position.z);
+            return;
+        }
+        
+        transform.position = Vector2.Lerp(toFollow.position, transform.position, smoothing);
     }
 }
