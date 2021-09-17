6b32635bc86f727f2d6c0d75c9129ecb2ae1decd
Sam Nilsson <sam64114208@gmail.com>
2021-09-17 11:20:03 +0200

    Final improvments

--- DialogProject/Assets/Scripts/Environment/DayCycle.cs
+++ DialogProject/Assets/Scripts/Environment/DayCycle.cs
@@ -3,9 +3,20 @@ using UnityEngine;
 public class DayCycle : MonoBehaviour
 {
     [SerializeField] private float rotateSpeed = 1f;
+    private Transform sunOrMoon;
 
     private void FixedUpdate()
     {
         transform.rotation *= Quaternion.Euler(0, 0, rotateSpeed);
+        sunOrMoon.rotation *= Quaternion.Euler(0, 0, -rotateSpeed);
+    }
+
+    private void Awake()
+    {
+        foreach (Transform t in GetComponentsInChildren<Transform>())
+        {
+            if (t == transform) continue;
+            sunOrMoon = t;
+        }
     }
 }
